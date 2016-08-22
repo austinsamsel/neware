22182526a603ab3889e0ac50473668f0a1d4bccc
austin samsel <austinsamsel@gmail.com>
2016-08-22 18:25:39 -0400

    add normalize and a just a few CSS rules

--- components/ChannelForm.js
+++ components/ChannelForm.js
@@ -5,6 +5,14 @@ let input;
 const ChannelForm = (props) => (
   <div>
     <div>
+      <p>
+        If you want to do anything on the internet you need to create an account, sign up and login. If your password is any good, you probably can't remember it. So when you find yourself on a public computer or a friend's device and your all logged out... but you really want to save a note for later... you can use NEWARE.
+      </p>
+      <p>
+        <strong>How it works:</strong> You create a memorable channel, which is just a URL, and save your note to it. No log in, no sign up, no account necessary. Your note will <strong>not</strong> be private, but you can access it later at the URL you created. All notes are posted for 24 hours and after that, they get removed.
+      </p>
+    </div>
+    <div>
       create or go to a channel:
     </div>
     {console.log(props)}
