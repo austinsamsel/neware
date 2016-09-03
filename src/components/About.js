b337e395c4800fffeb1fbc65b10253b1ef472fb9
austin samsel <austinsamsel@gmail.com>
2016-09-03 18:50:41 -0400

    update about note

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,15 @@ const About = () => (
       These days if you want to do anything on the internet you need to create an account, sign up and login. If your password is any good, you probably can't remember it. So when you find yourself on a public computer or a friend's device... but you need to save a note for later... you can use NEWARE.
     </p>
     <p>
-      <strong>How it works:</strong> Create a memorable channel, which is just a URL, for example: neware.surge.sh/<strong>MYCHANNEL</strong>, and save your note to it. No log in, no sign up, no account necessary. Your note will <strong>not</strong> be private, but you can access it later. 
+      <strong>How it works:</strong> Create a memorable channel, which is just a URL, for example: 
+    </p>
+
+    <p>
+    {window.location.href}<strong>MYCHANNEL</strong> 
+    </p>
+
+    <p>
+    and save your note to it. No log in, no sign up, no account necessary. Your note will <strong>not</strong> be private, but you can access it later. 
     </p>
     <p>
       All notes are available for 24 hours and after that, they get removed.
