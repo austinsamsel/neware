45c0b2b80b4fea669766dd40d0ab75432f121ca7
austin samsel <austinsamsel@gmail.com>
2016-09-03 18:27:06 -0400

    make 24 hr notice own paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,10 @@ const About = () => (
       These days if you want to do anything on the internet you need to create an account, sign up and login. If your password is any good, you probably can't remember it. So when you find yourself on a public computer or a friend's device... but you need to save a note for later... you can use NEWARE.
     </p>
     <p>
-      <strong>How it works:</strong> Create a memorable channel, which is just a URL, for example: neware.surge.sh/<strong>MYCHANNEL</strong>, and save your note to it. No log in, no sign up, no account necessary. Your note will <strong>not</strong> be private, but you can access it later. All notes are posted for 24 hours and after that, they get removed.
+      <strong>How it works:</strong> Create a memorable channel, which is just a URL, for example: neware.surge.sh/<strong>MYCHANNEL</strong>, and save your note to it. No log in, no sign up, no account necessary. Your note will <strong>not</strong> be private, but you can access it later. 
+    </p>
+    <p>
+      All notes are available for 24 hours and after that, they get removed.
     </p>
     <p>
       Made by <a style={{color:'fuchsia'}} href="https://hightops.co">High Tops</a>.
