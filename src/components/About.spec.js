baf1ff2f6e779e8f12f3e80ec887a97375552fe5
austin samsel <austinsamsel@gmail.com>
2016-09-10 12:28:53 -0400

    fix comments

--- src/components/About.spec.js
+++ src/components/About.spec.js
@@ -3,17 +3,13 @@ import { shallow } from 'enzyme';
 import About from './About';
 import ReactDOM from 'react-dom';
 
-// full rendering uses ReactDOM
+// smoke test:
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<About />, div);
 });
 
 // shallow rendering with Enzyme
-it('renders without crashing', () => {
-  shallow(<About />);
-});
-
 it('renders the tagline', () => {
   const wrapper = shallow(<About />);
   const tagline = 'Save it for later from anywhere.'
--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
 
-// full rendering with mounting
+// smoke test:
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
+// shallow rendering, testing output:
 it('renders the title', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.contains('NEWARE')).toEqual(true);
